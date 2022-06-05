4597f2876b43b14e77b7b02b32db13bd3cc5f1c9
timmyu32 <35674082+timmyu32@users.noreply.github.com>
2022-06-04 21:41:36 -0400

    Added various Components and Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
 
 function App() {
   return (
     <div className="App">
+      <Product/>
     </div>
   );
 }
---
+++ frontend/src/pages/ProductList.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
+
+
+const Container = styled.div`
+
+`;
+
+const Container2 = styled.div`
+    display: flex;
+`;
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Title = styled.h1`
+    margin: 20px;
+`;
+
+const Filter = styled.div`
+    margin-right: 20px;
+`;
+
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+`;
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 10px;
+`;
+
+const Option = styled.option`
+
+`;
+
+
+const ProductList = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Announcement/>
+        <Title>Dress Shirts</Title>
+        <FilterContainer>
+            <Container2>
+            <Filter><FilterText>Filter Products</FilterText></Filter>
+            <Select>
+                <Option disabled selected>
+                    Color
+                </Option>
+                <Option>White</Option>
+                <Option>Black</Option>
+                <Option>Blue</Option>
+                <Option>Yellow</Option>
+                <Option>Green</Option>
+            </Select>
+            <Select>
+                <Option disabled selected>
+                    Size
+                </Option>
+                <Option>XS</Option>
+                <Option>S</Option>
+                <Option>M</Option>
+                <Option>L</Option>
+                <Option>XL</Option>
+            </Select>
+            </Container2>
+            <Container2>
+                <Filter><FilterText>Sort Products</FilterText></Filter>
+                <Select>
+                    <Option selected>
+                        Newest
+                    </Option>
+                    <Option>Price (asc)</Option>
+                    <Option>Price (decc)</Option>
+                </Select>
+            </Container2>
+        </FilterContainer>  
+        <Products/>
+        <Newsletter/>
+        <Footer/>
+
+      
+    </Container>
+  )
+}
+
+export default ProductList
